eb43637966550f41b7692f0ba9e1085425a063c8
Jack Farley <or4e3dsw@bytes.nz>
2016-07-19 12:28:37 +0100

    - Added ability to save forms - Added tiered logging - Created function to extract metadata from files - Created form to id mapping - Changed to use 40 character random string for id - Got file watching working

--- index.js
+++ index.js
@@ -5,9 +5,26 @@ var Promise = require('promise');
 var path = require('path');
 var fs = require('fs');
 var util = require('util');
+var mkdirp = require('mkdirp-promise');
+var process = require('process');
+var log = require('npmlog');
+
+var commander = require('commander')
+  .option('-d, --debug', 'Debug logging')
+  .option('-v, --verbose', 'Verbose logging')
+  .parse(process.argv);
+
+var access = Promise.denodeify(fs.access);
+var writeFile = Promise.denodeify(fs.writeFile);
 
 var app = express();
 var forms;
+
+if (commander.debug) {
+  log.level = 'debug';
+} else if (commander.verbose) {
+  log.level = 'verbose';
+}
 
 require('promise/lib/rejection-tracking').enable();
 
@@ -16,7 +33,7 @@ var srcFolder = path.resolve('src/forms');// .../<app>/<form>.xhtml
 // Defines where forms will be published to
 var pubFolder = path.resolve('WEB-INF/resources/forms'); // .../<app>/<form>/form/form.xhtml
 
-console.log('folders are', srcFolder, pubFolder);
+log.verbose('folders are', srcFolder, pubFolder);
 
 // Use the simple text body parser for POST requests
 // TODO The request MUST have a Content-Type: application/xml header.
@@ -28,12 +45,12 @@ app.use(bodyParser.text({
 // Get form for form builder
 app.get('/crud/orbeon/builder/data/:id/data.xml', function getForm(req, res, next) {
   // Check id is in forms
-  console.log('request for form', req.params);
+  log.silly('request for form', req.params);
   if (forms.forms[req.params.id]) {
-    console.log('form no exist', forms.forms[req.params.id]);
+    log.verbose('form exist', forms.forms[req.params.id]);
     res.sendFile(forms.forms[req.params.id].file, next);
   } else {
-    console.log('form no exist', req.params.id);
+    log.warn('form no exist', req.params.id);
     res.status(404);
     next();
   }
@@ -48,7 +65,7 @@ app.post('/search/orbeon/builder', function handleBuilderSearch(req, res, next) 
       return;
     }
 
-    console.log('parsed xml', util.inspect(result, { depth: null }));
+    log.silly('parsed xml', util.inspect(result, { depth: null }));
 
     if (result.search) {
       var i, last, size, number,
@@ -145,7 +162,7 @@ app.post('/search/orbeon/builder', function handleBuilderSearch(req, res, next) 
       var builder = new xml2js.Builder();
       var responseXml = builder.buildObject(response);
 
-      console.log(responseXml);
+      log.silly(responseXml);
       res.type('application/xml').send(responseXml);
     }
 
@@ -154,14 +171,85 @@ app.post('/search/orbeon/builder', function handleBuilderSearch(req, res, next) 
 });
 
 // Save Form
-app.post('/crud/orbeon/builder/data/:id/data.xml', function saveForm(req, res, next) {});
+// /crud/orbeon/builder/data/419a29bc6ecad15209dbfdbc7ee6c3f8e53e88e7/data.xml
+app.put('/crud/orbeon/builder/data/:id/data.xml', function saveForm(req, res, next) {
+  // Extract the metadata from the new form
+  return forms.extractMetadata(req.body).then(function(metadata) {
+    // Check the folder exists
+    var formPath = path.join(srcFolder, metadata['application-name']);
+
+    return access(formPath, fs.W_OK).catch(function appFolderIssue(err) {
+      switch(err.code) {
+        case 'ENOENT': // Does not exist
+          // Try creating the directory
+          return mkdirp(formPath);
+        default:
+          return Promise.reject(err);
+      }
+    }).then(function saveHasFolder() {
+      var oldId;
+
+      // Save the xml
+      formPath = path.join(formPath, metadata['form-name'] + '.xhtml');
+
+      // Check if id is already used
+      if (forms.forms[req.params.id]) {
+        var form = forms.forms[req.params.id];
+
+        // Check that it is still the same form
+        if (metadata['application-name'] !== form['application-name']
+            || metadata['form-name'] !== form['form-name']) {
+          // If not, move the old form to a new id
+          var newId = forms.newId(40);
+          
+          log.warn('moving', form['application-name'], form['form-name'],
+              'from', req.params.id, 'to', newId);
+          
+          forms.forms[newId] = form;
+          // Fix new mapping
+          forms.setIdMapping(newId, form['application-name'], form['form-name']);
+        }
+      }
+      
+      // Check if form already has id
+      if ((oldId = forms.getFormId(metadata['application-name'],
+          metadata['form-name']))) {
+        // Check it is the same, if not move it
+        if (oldId !== req.params.id) {
+          log.warn('changing mapping from', oldId, 'to', req.params.id);
+          forms.forms[req.params.id] = forms.forms[oldId];
+          delete forms.forms[oldId];
+          forms.setIdMapping(req.params.id, metadata['application-name'],
+              metadata['form-name']);
+        }
+      }
+      
+      // Set id to form mapping so when the other data is
+      // loaded by the watcher it uses the correct id
+      forms.setIdMapping(req.params.id, metadata['application-name'],
+          metadata['form-name']);
+
+      log.info('form', metadata['application-name'] + '/'
+          + metadata['form-name'], 'saved (' + req.params.id + ')');
+      return writeFile(formPath, req.body);
+    }).then(function() {
+      res.status(201).end();
+    });
+    next();
+  }).catch(function(err) {
+    console.error(err.message, err.stack);
+    res.status(500).send(err.message);
+    next();
+  })
+});
 
 // Publish Form
 //app.post();
 
 // Log everything
 app.all('*', function(req, res, next) {
-  console.log(new Date(), 'Got new request', req.method, req.url, req.headers, req.body);
+  log.verbose(new Date(), 'Got new request', req.method, req.url);
+  log.silly(req.headers, req.body);
   next();
 });
 
@@ -170,8 +258,14 @@ require('./lib/forms-watcher')({
 }).then(function(formsObject) {
   forms = formsObject;
 
+  Object.keys(forms.forms).forEach(function(id) {
+    var form = forms.forms[id];
+    log.verbose(id, ':', form['application-name'], form['form-name'],
+        form.file, form.title);
+  });
+
   app.listen('6483');
-  console.log('CRUD API instance now listening on 6483');
+  log.info('CRUD API instance now listening on 6483');
 }).catch(function(err) {
   console.error(err.message, error.stack);
 });
